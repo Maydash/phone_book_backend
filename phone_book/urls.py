7b40e7df2d2ee90d3e4b781685790d2ddaaaf394
Maydash <maydashdowletow@gmail.com>
2023-06-14 17:57:22 +0500

    14.06.2023

--- phone_book/urls.py
+++ phone_book/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('api/getphonebooklist', PhoneBookList.as_view(), name='phone-book-list-api'),
+    path('api/getcomingbirthdays', get_coming_birthdays, name='get_coming_birthdays-api'),
+    path('api/getbirthdaytoday', birthday_today, name='get_birthday_today-api'),
     ]
--- phone_book/views.py
+++ phone_book/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from datetime import date, datetime
@@ -12,6 +13,7 @@ class PhoneBookList(ListAPIView):
 
 
 #doglan gune 1 hepde galsa spisoga goshulyar
+@api_view(['GET'])
 def get_coming_birthdays(request):
     coming_birthdays = []
     today = datetime.date.today()
@@ -20,8 +22,23 @@ def get_coming_birthdays(request):
     days_since_start = (today - start_of_year)
     for item in PhoneBook.objects.all():
         my_date = item.birthday
-        days_passed = my_date-date(my_date.year, 1, 1)
+        days_passed = my_date - date(my_date.year, 1, 1)
         if 0 > (days_since_start - days_passed).days < 7:
             coming_birthdays.append(item)
     serializer = PhoneBookSerializer(coming_birthdays, context={'request': request}, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def birthday_today(request):
+    birthday_today = []
+    today = datetime.date.today()
+    start_of_year = datetime.date(today.year, 1, 1)
+    #yylyn bashyndan bari shu gune cenli nace gun gecdi
+    days_since_start = (today - start_of_year)
+    for item in PhoneBook.objects.all():
+        my_date = item.birthday
+        days_passed = my_date - date(my_date.year, 1, 1)
+        if days_since_start.days == days_passed.days:
+            birthday_today.append(item)
+    serializer = PhoneBookSerializer(birthday_today, context={'request': request}, many=True)
     return Response(serializer.data)
